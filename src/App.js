28a2f96cd2b32f64e0cb5628816959063dd2e8ec
Santiago_Diaz <santiago.diaz11d@gmail.com>
2020-05-05 10:49:55 +1000

    Fixing the problems

--- src/App.js
+++ src/App.js
@@ -2,14 +2,29 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+constructor() {
+  super();
+
+  this.state = {counter:0};
+}
+
+handleChange = (event) => {
+  const cadena = event.target.value
+  // console.log(cadena);
+  this.setState({
+    counter: cadena.length
+  })
+}
+
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.handleChange}></textarea>
+    <div className="counter">{this.state.counter}</div>
       </div>
     );
   }
 }
 
 export default App;
+
